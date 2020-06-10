b0e62ba3d0afe4918d11bb67c6a0d1b6b515a0c0
OiTAvO <otavio.porto@fatec.sp.gov.br>
2020-06-10 01:54:40 -0300

    closes #20

---
+++ src/ders/d.pt/writeln.cozum.d
@@ -0,0 +1,32 @@
+Ddoc
+
+$(COZUM_BOLUMU writeln e write)
+
+$(OL
+
+$(LI
+Um forma é passar um parâmetro entre os outros:
+
+---
+    writeln("Hello, World!", " ", "Hello, fish!");
+---
+
+)
+
+$(LI
+$(C write) também pode ter vários parâmetros:
+
+---
+    write("one", " two", " three");
+---
+
+)
+
+)
+
+Macros:
+        SUBTITLE=writeln e write Solutions
+
+        DESCRIPTION=Programando em D soluções de exercícios: writeln e write
+
+        KEYWORDS=programando em d tutorial writeln e write exercício solução
